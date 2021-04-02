b4e76c389696fdbdb40e5be021f5975164e7dae0
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-04-02 11:28:33 +0300

    Delete Routine.rb

--- Routine.rb
+++
@@ -1,8 +0,0 @@
-100000000000000000.times do
-    puts "Despite all this activity Churchill’s daily routine changed little during these years. He awoke about 7:30 a.m. and remained in bed for a substantial breakfast and reading of mail and all the national newspapers. For the next couple of hours, still in bed, he worked, dictating to his secretaries.
-At 11:00 a.m., he arose, bathed, and perhaps took a walk around the garden, and took a weak whisky and soda to his study.
-At 1:00 p.m. he joined guests and family for a three-course lunch. Clementine drank claret, Winston champagne, preferable Pol Roger served at a specific temperature, port brandy and cigars. When lunch ended, about 3:30 p.m. he returned to his study to work, or supervised work on his estate, or played cards or backgammon with Clementine.
-At 5:00 p.m., after another weak whisky and soda, he went to be for an hour and a half. He said this siesta, a habit gained in Cuba, allowed him to work 1 1/2 days in every 24 hours. At 6:30 p.m. he awoke, bathed again, and dressed for dinner at 8:00 p.m.
-Dinner was the focal-point and highlight of Churchill’s day. Table talk, dominated by Churchill, was as important as the meal. Sometimes, depending on the company, drinks and cigars extended the event well past midnight. The guests retired, Churchill returned to his study for another hour or so of work.
-"
-end
